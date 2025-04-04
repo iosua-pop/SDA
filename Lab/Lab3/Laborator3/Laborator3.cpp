6c90185343a5cb9f876a42d930ad33f16c2c4e5b
iosua-pop <pop.iosua@yahoo.com>
2025-04-04 03:55:54 +0300

    Laborator 3

---
+++ Lab/Lab3/Laborator3/Laborator3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+#include "TestExtins.h"
+#include "TestScurt.h"
+
+using namespace std;
+
+
+int main() {
+	testAll();
+	cout << "Teste scurete finalizate\n";
+	testAllExtins();
+	cout << "Teste extinse finalizate\n";
+}
